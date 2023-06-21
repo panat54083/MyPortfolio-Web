8de881065b3f3435d9af15f5cf79e70e6054f1cd
Panat Siriwongtrakool <s6201012610052@email.kmutnb.ac.th>
2023-06-21 11:56:01 +0700

    Initial Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import css from "./styles/app.module.scss";
 
 const App = () => {
-  return (
-    <div>Hello World</div>
-  )
-}
+    return (
+        <div className={`bg-primary ${css.container}`}>
+            <div>Hello World</div>
+        </div>
+    );
+};
 
-export default App
+export default App;
